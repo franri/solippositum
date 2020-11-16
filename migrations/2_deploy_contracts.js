e046a0ac892fd7115f643062ae70a27bec6dc9a5
franri <franrizzo.98@gmail.com>
2020-11-15 22:09:24 -0300

    merge

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -8,7 +8,7 @@ const events = [
 ];
 
 module.exports = function(deployer) {
-  deployer.deploy(Apuesta, "Partido", events, "0x19fE5F5728f52A71439De01170d47B2D1870967B");
+  deployer.deploy(Apuesta, "Partido", events, "0xd574B00686bE215F307BD64d761fF1F34352FB6c");
   deployer.deploy(ApuestaManager);
   // deployer.deploy(Commons);
 };
