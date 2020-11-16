156c78395bfbb4b10b568528cb5b13766ab746c5
maxipalay <maximilianopalay@gmail.com>
2020-11-15 23:18:06 -0300

    ya se puede seleccionar un evento, poner el amount y apostar

--- app/src/components/ContratoApuestas.js
+++ app/src/components/ContratoApuestas.js
@@ -5,8 +5,11 @@ import EventItem from "./EventItem";
 const { ContractForm } = newContextComponents;
 
 class ContratoApuestas extends React.Component {
-    state = { dataKeyName: null , dataKeyEvents: null };
-   
+    state = { dataKeyName: null , dataKeyEvents: null, amountToBet:'', selection:'' };
+    handleChangeAmount = this.handleChangeAmount.bind(this);
+    handleChangeSelection = this.handleChangeSelection.bind(this);
+
+
     componentDidMount() {
       const { drizzle } = this.props;
       const contract = drizzle.contracts.Apuesta;
@@ -14,7 +17,14 @@ class ContratoApuestas extends React.Component {
       let dataKeyEvents = contract.methods["getEvents"].cacheCall(); // declare this call to be cached and synchronized
       this.setState({ dataKeyName, dataKeyEvents });
     }
-   
+    handleChangeAmount(event) {
+      this.setState({amountToBet: event.target.value});
+      console.log(event.target.value);
+    }
+    handleChangeSelection(event) {
+      this.setState({selection: event.target.value});
+      //console.log(event.target.value);
+    }
     render() {
       const { Apuesta } = this.props.drizzleState.contracts;
       const contractName = Apuesta.nombre[this.state.dataKeyName]; // if displayData (an object) exists, then we can display the value below
@@ -31,13 +41,24 @@ class ContratoApuestas extends React.Component {
                 ))
             }
             <h3>Apostar</h3>
-            <ContractForm
+            <div>
+              <input type="text" value={this.state.amountToBet} onChange={this.handleChangeAmount} />
+              <select value={this.state.selection} onChange={this.handleChangeSelection}>
+              {contractEvents &&
+                //console.log(contractEvents)
+                contractEvents.value.map((e, i) => (
+                    <option value={e.id} key={i}>{e.id}</option>
+                ))}
+              </select>
+            {/*<ContractForm 
               drizzle = {this.props.drizzle}
               contract="Apuesta"
               method="registerBid"
-              sendArgs={{from:this.props.drizzleState.accounts[0], value:2000000000000}}
+              sendArgs={{from:this.props.drizzleState.accounts[0], value:this.state.amountToBet}}
               labels={["Evento"]}
-            />
+            />*/}
+            </div>
+            
         </div>
       )
     }
@@ -45,7 +66,7 @@ class ContratoApuestas extends React.Component {
    
    export default ContratoApuestas
 
-{/* 
+/* 
 export default ({ drizzle, drizzleState }) => {
   // destructure drizzle and drizzleState from props
   return (
@@ -73,4 +94,4 @@ export default ({ drizzle, drizzleState }) => {
     </div>
   );
 };
-*/}
+*/
